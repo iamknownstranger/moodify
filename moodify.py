ca30ec5e3edd6985adfb03e84d187a7fc2a616c5
Chandra Sekhar Mullu <47244469+iamknownstranger@users.noreply.github.com>
2021-09-09 17:10:26 +0530

    Create moodify.py

---
+++ moodify.py
@@ -0,0 +1,44 @@
+import streamlit.components.v1 as components
+import streamlit as st
+from spotify_client import track_meta_data, data, plt
+# st.set_page_config(page_title="Moodify", layout="wide")
+st.title("**Sekhar's recently played songs**")
+# st.dataframe(track_meta_data)
+# st.dataframe(data)
+
+
+url = 'https://open.spotify.com/embed/album/'
+for index, track in data.iterrows():
+    data_container = st.container()
+    
+    with data_container:
+        images, track_data = st.columns([2, 6])
+        images.image(track.images)
+        with track_data:
+            # track_name, album_name = st.columns([3, 3])
+            st.header(f'**{track.track_name}**')
+            st.subheader(f'from **{track.album_name}**')
+
+            with st.expander("Play on Spotify"):
+                components.iframe(url + track.album_uri)
+ 
+st.header("Sekhar's **moodify** chart")
+st.pyplot(plt)
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            footer:after {
+                content:"Built with 💓 by Chandra Sekhar Mullu"; 
+                visibility: visible;
+                display: block;
+                position: relative;
+                #background-color: red;
+                padding: 5px;
+                top: 2px;
+            }
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
